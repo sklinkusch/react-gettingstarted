6fc48ec72763c237c1e33f3b237922d274d8691b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-01 12:59:17 +0200

    changed image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import stefan from "./Stefan.jpg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={stefan} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
